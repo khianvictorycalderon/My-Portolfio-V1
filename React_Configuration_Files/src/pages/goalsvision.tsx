078f35c40cfaca60f29cb33d44782e268e6140f0
Khian Calderon <ckhianvictory@gmail.com>
2024-12-23 01:01:46 +0800

    Reorgranized for Hosting

---
+++ React_Configuration_Files/src/pages/goalsvision.tsx
@@ -0,0 +1,16 @@
+export default function GoalsAndVision() {
+    return (
+        <div className="goals-and-vision p-4 text-light fs-5">
+            <h1>Goals:</h1>
+            <div className="dent-1">
+                <p>As a dedicated freelancer specializing in both front-end and back-end web development, my primary goal is to deliver high-quality, efficient, and user-friendly web solutions tailored to meet the unique needs of each client. I aim to leverage my expertise in technologies such as React, PHP, and MySQL to create seamless, dynamic, and responsive websites. By continuously enhancing my skills and staying updated with the latest industry trends, I strive to provide innovative and reliable web development services that help businesses grow and succeed in the digital landscape. Ultimately, my goal is to build trusts with clients by consistently meeting their expectations and contributing to their success through exceptional web development solutions.</p>
+                <p>My goal is to help businesses, whether small or large, achieve their full potential and become successful in their respective markets. By offering tailored web development solutions that enhance their online presence, streamline their operations, and engage their customers, I aim to empower businesses to thrive in the competitive digital environment. I am committed to understanding the unique challenges and goals of each business, and I take pride in delivering solutions that drive growth, efficiency, and success.</p>
+            </div>
+            <hr/>
+            <h1>Vision:</h1>
+            <div className="dent-1">
+                <p>My vision is to achieve success in my career by securing a stable, fulfilling job that allows me to utilize and expand my skills in web development. In parallel, I aim to maintain a part-time freelancing practice where I can continue to explore creative projects, help businesses grow, and stay connected with the dynamic world of technology. By balancing a stable career and part-time freelancing, I aspire to enhance my professional growth, deliver exceptional value to clients, and achieve a harmonious blend of stability and creativity in my work life.</p>
+            </div>
+        </div>
+    )
+}
---
+++ React_Configuration_Files/src/pages/privacypolicy.tsx
@@ -0,0 +1,14 @@
+export default function PrivacyPolicy() {
+    return (
+        <div className="privacy-policy fs-4 text-light p-4">
+            <h1>Privacy Policy</h1>
+            <hr/>
+            <div className="dent-1">
+            <p>I am committed to respecting your privacy and ensuring the security of any personal information you may provide while visiting my website. I do not collect any personal information from visitors to my website. Your privacy is of utmost importance to me, and I take every measure to protect it.</p>
+            <p>When you visit my website, no personal data, such as your name, email address, or any other identifying information, is collected or stored. I do not use cookies, tracking technologies, or any other means to gather information about your visit. You can browse my website with confidence, knowing that your privacy is safeguarded.</p>
+            <p>If you choose to contact me via email or other communication methods, any information you provide will be used solely for the purpose of responding to your inquiry and will not be shared with any third parties without your explicit consent.</p>
+            <p>Should you have any questions or concerns about my privacy policy, please feel free to contact me directly. Your trust and confidence are important to me, and I am committed to maintaining the highest standards of privacy and security for all visitors to my website.</p>
+            </div>
+        </div>
+    )
+}
